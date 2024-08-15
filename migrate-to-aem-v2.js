5f3485308e58677b4526d2373db4352fc0d6997c
pulgupta <pulgupta@adobe.com>
2024-08-15 14:41:28 +0530

    added js file for upload

--- migrate-to-aem-v2.js
+++ migrate-to-aem-v2.js
@@ -1,22 +1,58 @@
 const DirectBinary = require('@adobe/aem-upload');
+const fs = require('fs');
+const {join} = require("node:path");
 
-// URL to the folder in AEM where assets will be uploaded. Folder
-// must already exist.
 const targetUrl = 'https://author-p111055-e1081044.adobeaemcloud.com/content/dam/ibm';
+const startingDirectory = '/Users/pulgupta/Library/CloudStorage/OneDrive-Adobe/Dox-Content/contract-tests/contract-tests';
+const uploadFiles = [];
 
-// list of all local files that will be uploaded.
-const uploadFiles = ['/Users/pulgupta/Library/CloudStorage/OneDrive-Adobe/Dox-Content/6k_files/6k_topics/gasb-qa_utf8'];
+// Traverse files and prepare data for upload
+function prepareFileData(dir) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = join(dir, file);
+        const fileStat = fs.statSync(filePath);
+        if (fileStat.isDirectory()) {
+            prepareFileData(filePath);
+        } else if (file !== '.DS_Store') {
+            uploadFiles.push({
+                fileName: file,
+                filePath: filePath,
+                fileSize: fileStat.size
+            });
+        }
+    }
+}
+
+prepareFileData(startingDirectory);
+console.log("Uploading files...");
 
 const upload = new DirectBinary.DirectBinaryUpload();
 const options = new DirectBinary.DirectBinaryUploadOptions()
     .withUrl(targetUrl)
     .withUploadFiles(uploadFiles)
-    .withMaxConcurrent(10);
-
-upload.uploadFiles(options)
-    .then(result => {
-        console.log(result)
-    })
-    .catch(err => {
-        console.error(err)
+    .withMaxConcurrent(10)
+    .withHttpOptions({
+        headers: {
+            Authorization: `Basic ${Buffer.from('testadmin:testadmin').toString('base64')}`
+        }
     });
+
+upload.on('filestart', data => {
+    const { fileName } = data;
+    console.log(`Started uploading ${fileName}`);
+});
+upload.on('fileprogress', data => {
+    const { fileName, transferred } = data;
+    console.log(`Uploading ${fileName}`)
+});
+upload.on('fileend', data => {
+    const { fileName } = data;
+    console.log(`Uploaded successfully ${fileName}`)
+});
+upload.on('fileerror', data => {
+    const { fileName, errors } = data;
+    console.log(`Error in uploading ${fileName}`)
+});
+
+upload.uploadFiles(options);
