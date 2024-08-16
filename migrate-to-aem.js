8d0a24cd75f48338b139b0027cd07a6d10f97dcc
pulgupta <pulgupta@adobe.com>
2024-08-16 10:39:43 +0530

    added concurrent option

--- migrate-to-aem.js
+++ migrate-to-aem.js
@@ -3,10 +3,11 @@ const {
     FileSystemUpload
 } = require('@adobe/aem-upload');
 const credentials = Buffer.from('testadmin:testadmin').toString('base64')
-const options = new FileSystemUploadOptions()
-    .withUrl('https://author-p111055-e1081044.adobeaemcloud.com/content/dam/ibm')
+const options = new FileSystemUploadOptions().from()
+    .withUrl('https://author-p111055-e1081044.adobeaemcloud.com/content/dam/ibm/4k')
     .withDeepUpload(true)
     .withMaxUploadFiles(50000)
+    .withMaxConcurrent(20)
     .withHttpOptions({
         headers: {
             Authorization: `Basic ${credentials}`
@@ -16,7 +17,7 @@ async function run() {
     // upload a single asset and all assets in a given folder
     const fileUpload = new FileSystemUpload();
     await fileUpload.upload(options, [
-        '/Users/pulgupta/Library/CloudStorage/OneDrive-Adobe/Dox-Content/6k_files/6k_topics/gasb-qa_utf8'
+        '/Users/pulgupta/Library/CloudStorage/OneDrive-Adobe/Dox-Content/4k_files/gasb-cig_utf8/gasb-cig_utf8'
     ]);
 }
 run();
